0d5c257497779bbe31c48698b5518a56f891b083
Jeremy Davis <jdavis@paraport.com>
2017-09-07 14:39:18 -0700

    Working EF db

--- MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -9,16 +9,13 @@ namespace MvcMusicStore.Controllers
 {
     public class StoreController : Controller
     {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+        
         //
         // GET: /Store/
         public ActionResult Index()
         {
-            var genres = new List<Genre>
-            {
-                new Genre {Name = "DarkWave"},
-                new Genre {Name = "Jazzhands"},
-                new Genre {Name = "Metal"}
-            };
+            var genres = storeDB.Genres.ToList();
 
             return View(genres);
         }
@@ -26,14 +23,17 @@ namespace MvcMusicStore.Controllers
         // GET: /Store/Browse
         public ActionResult Browse(string genre)
         {
-            var genreModel = new Genre { Name = genre };
+            var genreModel = storeDB.Genres.Include("Albums")
+                .Single(g => g.Name == genre);
+
             return View(genreModel);
         }
         //
         // GET: /Store/Details
         public ActionResult Details(int id)
         {
-            var album = new Album { Title = "Album " + id };
+            var album = storeDB.Albums.Find(id);
+
             return View(album);
         }
     }
