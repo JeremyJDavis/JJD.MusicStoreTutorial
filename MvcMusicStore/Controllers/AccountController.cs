201a4812566c48e6407a158102be94ce54d62b46
Jeremy Davis <jdavis@paraport.com>
2017-09-12 05:57:22 -0700

    adding acct controls

--- MvcMusicStore/Controllers/AccountController.cs
+++ MvcMusicStore/Controllers/AccountController.cs
@@ -8,9 +8,9 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using MvcMusicStore.Models;
+using WebApplication1.Models;
 
-namespace MvcMusicStore.Controllers
+namespace WebApplication1.Controllers
 {
     [Authorize]
     public class AccountController : Controller
--- MvcMusicStore/Models/AccountViewModels.cs
+++ MvcMusicStore/Models/AccountViewModels.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace MvcMusicStore.Models
+namespace WebApplication1.Models
 {
     public class ExternalLoginConfirmationViewModel
     {
